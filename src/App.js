bf9c0a5daba32c427a008f4c7b60eb53d517b10c
EmanuelGenially <emanuel@MacBook-Pro-de-Emanuel.local>
2018-10-15 14:50:57 +0200

    feat: change alt

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <div />
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo dos" />
           <p>
             Edit refactor a fix bug fix <code>src/App.js</code> and save to lul
             reload.
