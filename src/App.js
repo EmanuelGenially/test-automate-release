a4dd9090f9a2a3e590139d234151b9bfc65ad095
EmanuelGenially <emanuel@MacBook-Pro-de-Emanuel.local>
2018-10-15 14:42:29 +0200

    fix(bug): ejejje

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit refactor fix bug fix <code>src/App.js</code> and save to lul
+            Edit refactor a fix bug fix <code>src/App.js</code> and save to lul
             reload.
           </p>
           <br />
